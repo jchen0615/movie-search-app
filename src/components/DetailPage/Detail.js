89b0cc641f697de7a7b13b1fc13a16a214db9478
jchen0615 <45953090+jchen0615@users.noreply.github.com>
2020-02-16 00:01:13 -0500

    Updated GeneralInfo to be presentational

--- src/components/DetailPage/Detail.js
+++ src/components/DetailPage/Detail.js
@@ -13,10 +13,31 @@ const apiKey = "?api_key=0d727a18472e40764f879642668f20f9";
 class Detail extends Component{
     state= {
         initialRender: false,
-        similarMovies: null
+        similarMovies: null,
+        overview: null,
+        genre: null,
+        tagline: null,
+        hours: null,
+        minutes: null,
+        video: null
     }
 
-    getSimilarMovies(){
+    getMovieDetail=()=>{
+        axios.get("/movie/"+this.props.location.state.id+apiKey+"&append_to_response=videos")
+        .then( response =>{
+            this.setState({
+                overview: response.data.overview? response.data.overview : "No overview available",
+                genre: response.data.genres.length>0? response.data.genres[0].name:"No information available",
+                tagline: response.data.tagline? response.data.tagline:"",
+                hours: response.data.runtime>0? Math.floor(parseInt(response.data.runtime, 10)/60): null,
+                minutes: response.data.runtime>0? Math.floor(parseInt(response.data.runtime, 10)%60): null,
+                video: response.data.videos.results.find(element => element.type==="Trailer")? response.data.videos.results.find(element => element.type==="Trailer").key:null
+                //generalInfoLoaded: true,
+            })
+        })
+    }
+
+    getSimilarMovies=()=>{
         axios.get("/movie/"+this.props.location.state.id+"/similar"+apiKey)
         .then( response =>{
             const movieList = response.data.results.slice(0,4).map(movie =>{
@@ -37,17 +58,19 @@ class Detail extends Component{
     }
 
     componentDidMount(){      
+        this.getMovieDetail()
         this.getSimilarMovies()
     }
 
     componentDidUpdate(prevProps){
-        if(this.props.location.state.id!==prevProps.location.state.id)
+        if(this.props.location.state.id!==prevProps.location.state.id){
+            this.getMovieDetail()
             this.getSimilarMovies()
+        }
     }
 
     render(){
-      
-        if(!this.state.similarMovies){
+        if(!this.state.similarMovies || !this.state.video){
             return(
                 <Spinner/>
             )
@@ -58,7 +81,8 @@ class Detail extends Component{
                 <NavigationBar/>
                 <BackBtn goBack={this.props.history.goBack}/>
                 <GeneralInfo id = {this.props.location.state.id} title = {this.props.location.state.title} date = {this.props.location.state.date} 
-                    vote = {this.props.location.state.voteAverage} poster={this.props.location.state.poster}/>
+                    vote = {this.props.location.state.voteAverage} poster={this.props.location.state.poster} overview = {this.state.overview} genre = {this.state.genre}
+                    tagline = {this.state.tagline} hours = {this.state.hours} minutes = {this.state.minutes} video = {this.state.video}/>
                 <Reviews id = {this.props.location.state.id} list = {this.state.review_list}/>
                 <MovieGrid id = {this.props.location.state.id} movieType = "Similar" movies = {this.state.similarMovies}/>
             </div>
--- src/components/DetailPage/GeneralInfo/GeneralInfo.js
+++ src/components/DetailPage/GeneralInfo/GeneralInfo.js
@@ -1,75 +1,32 @@
 import React, { Component } from 'react';
 import './GeneralInfo.css';
-import axios from '../../../axios'
-import Spinner from '../../UI/Spinner/Spinner'
 const posterString = "http://image.tmdb.org/t/p/w342";
 const youtubeString = "https://www.youtube.com/embed/";
 const apiKey = "?api_key=0d727a18472e40764f879642668f20f9";
 
 class GeneralInfo extends Component{
    
-    state = {
-        hours: null,
-        minutes: null,
-        overview: null,
-        genre: null,
-        tagline: null,
-        video: null,
-        generalInfoLoaded: false,
-    }
-
-    getMovieDetail(){
-        axios.get("/movie/"+this.props.id+apiKey+"&append_to_response=videos")
-        .then( response =>{
-            console.log(response);
-            this.setState({
-                overview: response.data.overview? response.data.overview : "No overview available",
-                genre: response.data.genres.length>0? response.data.genres[0].name:"No information available",
-                tagline: response.data.tagline? response.data.tagline:"",
-                hours: response.data.runtime>0? Math.floor(parseInt(response.data.runtime, 10)/60): null,
-                minutes: response.data.runtime>0? Math.floor(parseInt(response.data.runtime, 10)%60): null,
-                video: response.data.videos.results.find(element => element.type==="Trailer")? response.data.videos.results.find(element => element.type==="Trailer").key:null,
-                generalInfoLoaded: true,
-            })
-        })
-    }
-
-    componentDidMount(){
-        this.getMovieDetail()
-    }
-   
-    componentDidUpdate(prevProps){
-        if(this.props.id!==prevProps.id)
-            this.getMovieDetail()
-    }
-
     render(){
 
-        if(!this.state.generalInfoLoaded){
-            return(
-                <Spinner/>
-            )
-        }
-
-        console.log(this.state.video);
         let video = <div className = "no_video">No trailer available</div>
-        if(this.state.video){
-            video = <iframe className = "video" title = "trailer" src={youtubeString+this.state.video} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        if(this.props.video){
+            video = <iframe className = "video" title = "trailer" key = {this.props.video} src={youtubeString+this.props.video} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
         }        
+
 
         return(
             <div>
                 <div className = 'overview-grid'>
                     <img className = "detail-cover" src = {posterString + this.props.poster} alt = {this.props.title}/>
                     <div className = 'detail-title'>{this.props.title}</div>
-                    <div className = 'detail-overview'>{this.state.overview}</div>
-                    <div className = 'tagline'>{this.state.tagline}</div>
+                    <div className = 'detail-overview'>{this.props.overview}</div>
+                    <div className = 'tagline'>{this.props.tagline}</div>
                 </div>
                 <div className = 'general-info'>
                     <ul className = 'info'>
-                        <li>Duration:&nbsp;{this.state.hours?this.state.hours+" H "+this.state.minutes+" MIN":"No Information Available"}</li>
+                        <li>Duration:&nbsp;{this.props.hours?this.props.hours+" H "+this.props.minutes+" MIN":"No Information Available"}</li>
                         <li>Released: &nbsp; {this.props.date}</li>
-                        <li>Genre: &nbsp; {this.state.genre}</li>
+                        <li>Genre: &nbsp; {this.props.genre}</li>
                         <li>Vote Rating Average: &nbsp;{this.props.vote}</li>
                     </ul>
                     {video}
